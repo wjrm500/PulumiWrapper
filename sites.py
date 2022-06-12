23a65e6832db66b96dbc4fd952733c2cde13ba64
William May <wjrm500@gmail.com>
2022-06-12 15:18:02 +0800

    Updating sites index

--- sites.py
+++ sites.py
@@ -3,5 +3,10 @@ from flask import Blueprint, render_template
 bp = Blueprint('sites', __name__, url_prefix = '/sites', template_folder = 'templates/sites')
 
 @bp.route('/', methods = ['GET'])
-def create_site():
+def index():
+    sites = [{'name': 'test'}]
+    return render_template('sites_index.html', sites = sites)
+
+@bp.route('/create', methods = ['GET'])
+def create():
     return render_template('create.html')
