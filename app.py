0fa4988db2d5eca955517c9d0dc699f6d30cb4a1
William May <wjrm500@gmail.com>
2022-06-13 23:03:36 +0800

    Further work

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sites
 
 app = Flask(__name__)
 app.register_blueprint(sites.bp)
+app.config['PULUMI_ORGANISATION'] = 'wjrm500'
+app.config['PULUMI_PROJECT_NAME'] = 'pulumi-wrapper'
 
 @app.route('/')
 def index():
--- sites.py
+++ sites.py
@@ -1,27 +1,49 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, current_app, render_template
+import pulumi
 import pulumi_aws as aws
+import pulumi.automation as auto
 
 bp = Blueprint('sites', __name__, url_prefix = '/sites', template_folder = 'templates/sites')
 
 def create_program(content):
     index_document = 'index.html'
     bucket = aws.s3.Bucket(
-        name = 'bucket',
+        'bucket',
         acl = 'public-read', # Access control list
         policy = (lambda path: open(path))('policy.json'),
         website = aws.s3.BucketWebsiteArgs(index_document = index_document)
     )
     aws.s3.BucketObject( # Creates the HTML document
+        'index',
         bucket = bucket.id,
         content = content,
         key = index_document
     )
+    pulumi.export('url', bucket.website_endpoint)
 
 @bp.route('/', methods = ['GET'])
 def index():
-    sites = [{'name': 'test'}]
+    sites = []
+    ws = auto.LocalWorkspace(
+        project_settings = auto.ProjectSettings(
+            name = current_app.config['PULUMI_ORGANISATION'],
+            runtime = 'python'
+        )
+    )
+    for stack_name in ws.list_stacks():
+        stack = auto.select_stack(stack_name = stack_name)
+        url = stack.outputs().get('url')
+        sites.append({'name': stack_name, 'url': url})
     return render_template('sites_index.html', sites = sites)
 
 @bp.route('/create', methods = ['GET'])
 def create():
+    def encapsulated_program():
+        return create_program('Abcd')
+    stack = auto.create_stack(
+        stack_name = 'test',
+        project_name = current_app.config['PULUMI_PROJECT_NAME'],
+        program = encapsulated_program
+    )
+    stack.up(on_output = print)
     return render_template('create.html')
